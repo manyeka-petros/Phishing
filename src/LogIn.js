c610736616b45ca5804d3763139f4f3bbc37b97e
petros-manyeka <manyekapetros29@gmail.com>
2022-12-23 22:43:49 -0800

    added controls in form

--- src/LogIn.js
+++ src/LogIn.js
@@ -4,11 +4,25 @@ const LogIn = () => {
   return (
     <div className='container'>
         <div className='row'>
-            <div>
+            <div className='col-md-6 offset-md-3 bordered round shadow p-4 mt-3'>
                 <div>
-                    <form >
+                    <h2>Log In</h2>
+                    <form className='form-group row mb-3'>
                         <input
+                        type= 'text'
+                        className='form-control m-3 p-2'
+                        name = 'username'
+                        value  
+                        placeholder = "username"
                         />
+                        <input
+                        type= 'text'
+                        className='form-control m-3 p-2'
+                        name = 'password'
+                        value  
+                        placeholder = 'password'
+                        />
+
 
                     </form>
                 </div>
