aa7bdcabb40b28e03ed1919ef6278b328e14ffed
petros-manyeka <manyekapetros29@gmail.com>
2022-12-23 22:13:59 -0800

    added form componet

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <header className="App-header">
         <Navbar/>
         <LogIn/>
+        
        
       </header>
     </div>
--- src/LogIn.js
+++ src/LogIn.js
@@ -3,6 +3,17 @@ import React from 'react'
 const LogIn = () => {
   return (
     <div className='container'>
+        <div className='row'>
+            <div>
+                <div>
+                    <form >
+                        <input
+                        />
+
+                    </form>
+                </div>
+            </div>
+        </div>
       
     </div>
   )
