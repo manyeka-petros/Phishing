a330a95920a658c599937ab78f1655e28b31ee34
petros-manyeka <manyekapetros29@gmail.com>
2022-12-23 22:08:47 -0800

    added navbar componet

---
+++ src/LogIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogIn = () => {
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
+
+export default LogIn
---
+++ src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav1'>
+        <div className='nav2'>
+            <h2>OWASP Juice Shop</h2>
+        </div>
+        <div className='nav3'>
+            <a href='#'>Account</a>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
