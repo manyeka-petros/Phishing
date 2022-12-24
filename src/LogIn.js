9d9318612952294abd82967237556e6dd92ed562
petros-manyeka <manyekapetros29@gmail.com>
2022-12-23 23:10:18 -0800

    i added color

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,13 +1,14 @@
 import React from 'react'
-
+import './LogIn.css'
 const LogIn = () => {
   return (
-    <div className='container'>
+    <div className='bg-dark'>
+    <div className='container '>
         <div className='row'>
-            <div className='col-md-6 offset-md-3 bordered round shadow p-4 mt-3'>
+            <div className='col-md-6 offset-md-4 bordered round shadow p-4 mt-3'>
                 <div>
-                    <h2>Log In</h2>
-                    <form className='form-group row mb-3'>
+                    <h2 className='text-white'>Log In</h2>
+                    <form className='form-group row mb-3 bg-secondary'>
                         <input
                         type= 'text'
                         className='form-control m-3 p-2'
@@ -22,11 +23,20 @@ const LogIn = () => {
                         value  
                         placeholder = 'password'
                         />
+                        <p className='text-white'>
+                            Forget your Account?
+
+                        </p>
+
+                        <button className='btn btn-secondary text-white '>Log In</button>
+                        <p className='text-white'>Remeber Me</p>
+                        <button className='btn btn-success W-70'>Log with google</button>
 
 
                     </form>
                 </div>
             </div>
+        </div>
         </div>
       
     </div>
